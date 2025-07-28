e9800d1a44c003c50cc9b8ef1af5b9d3316cdc3a
David Felipe Rendon Luna <biodfrl@gmail.com>
2025-07-28 12:29:06 -0600

    Documentar y corregir variables.

--- bin/irma_blast.sh
+++ bin/irma_blast.sh
@@ -2,66 +2,66 @@
 set -uo pipefail
 
 # ============================================
-# Configuración
+# DECLARAR VARIABLES
 # ============================================
 DB="/backup/DATABASES/NCBI/NT_VIRUSES/nt_viruses"
 OUTPUT="blast_best_hit_hsp.tsv"
 OUTPUT_SORT="blast_best_hit_hsp_sorted.tsv"
 
 # ============================================
-# Procesar cada archivo A_*.fasta
+# PROCESAR CADA ARCHIVO A_*.fasta
 # ============================================
 
 # Crear archivo con nombres de columnas
 echo "SEGMENT QUERY BITSCORE SUBJECT ALIG_LEN Q_LEN S_LEN PIDENT Q_START Q_END S_START S_END S_TITLE " | tr ' ' '\t' >"$OUTPUT"
 
+# Para cada fasta producido por IRMA
 for fasta in A_*.fasta
 do
-    # Extraer nombre del archivo sin el .fasta
-    base=$(basename "$fasta" .fasta)
+    # Extraer nombre del archivo sin la extensión
+    NAME=$(basename "$fasta" .fasta)
 
-    # Crear archivo temporal
-    tmp_file="${base}_raw.tsv"
+    # Crear nombre para archivo temporal
+    TMP="${NAME}_raw.tsv"
 
     echo "Procesando: $fasta"
 
-    # Buscar patrón en $base para obtener el número de segmento
-    case "$base" in
-        *PB2*) segment="1" ;;
-        *PB1*) segment="2" ;;
-        *PA*)  segment="3" ;;
-        *HA*)  segment="4" ;;
-        *NP*)  segment="5" ;;
-        *NA*)  segment="6" ;;
-        *MP*)  segment="7" ;;
-        *NS*)  segment="8" ;;
-        *)     echo "⚠️  Segmento no identificado en: $base"; segment="?" ;;
+    # Buscar patrón en $NAME para obtener el número de segmento
+    case "$NAME" in
+        *PB2*) SEGMENT="1" ;;
+        *PB1*) SEGMENT="2" ;;
+        *PA*)  SEGMENT="3" ;;
+        *HA*)  SEGMENT="4" ;;
+        *NP*)  SEGMENT="5" ;;
+        *NA*)  SEGMENT="6" ;;
+        *MP*)  SEGMENT="7" ;;
+        *NS*)  SEGMENT="8" ;;
+        *)     echo "⚠️  Segmento no identificado en: $NAME"; SEGMENT="?" ;;
     esac
 
-    # Ejecutar BLASTN 
+    # Ejecutar BLASTN, con NT_virus, sobre cadena plus. 
     blastn -query "$fasta" \
-	-db "$DB" \
-	-strand 'plus' \
-        -outfmt "6 qseqid bitscore saccver length qlen slen pident qstart qend sstart send stitle " \
-        -out "$tmp_file" \
-	-num_threads 26
+    -db "$DB" \
+    -strand 'plus' \
+    -out "$TMP" \
+    -outfmt "6 qseqid bitscore saccver length qlen slen pident qstart qend sstart send stitle " \
+    -num_threads 26 # Modificar si es requerido
 
-    # Obtener mejor subject con mejor mayor bitscore
-    best_subject=$( sort -k2,2nr "$tmp_file" | head -n1 | cut -f3 )
+    # Obtener mejor subject en función del mayor bitscore
+    best_subject=$( sort -k2,2nr "$TMP" | head -n1 | cut -f3 )
 
-    # Extraer todos los HSPs de ese best subject
-    awk -v best="$best_subject" -v seg="$segment" '$3 == best { print seg "\t" $0 }' "$tmp_file" >> "$OUTPUT"
+    # Extraer todos los HSPs de ese best subject. Guardar en archivo que ya tienen los nombres de columnas
+    awk -v best="$best_subject" -v seg="$SEGMENT" '$3 == best { print seg "\t" $0 }' "$TMP" >> "$OUTPUT"
 
     # Eliminar archivo temporal
-    rm "$tmp_file"
-
+    rm "$TMP"
 done
 
 # ============================================
-# Ordenar por segmento y por posicion de inicio en subject
+# ORDENAR POR SEGMENTO Y POR POSICIÓN DE INICIO EN SUBJECT
 # ============================================
 
-# Seleccionar columnas menos bitscore. 
+# Seleccionar columnas, excepto la de bitscore.
 # Ordenar por columna 1 y luego por columna 10 numéricamente. Respetar líneas en blanco.
 cut -f1,2,4-13 "$OUTPUT" | LC_ALL=C sort -t $'\t' -k1,1 -k10,10n | awk -F'\t' '
 BEGIN { OFS = FS; prev = "" }
@@ -71,7 +71,7 @@ BEGIN { OFS = FS; prev = "" }
     }
     print
     prev = $1
-} ' > "$OUTPUT_SORT"
+} ' > "$OUTPUT_SORT"s
 
 # Eliminar archivo sin ordenar
 rm "$OUTPUT"
